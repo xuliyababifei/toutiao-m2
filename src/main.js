b7c1bf94f03648f5f94251d03f4df30d9a6f42f1
it <1246442241@qq.com>
2022-06-30 23:40:17 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+// 引入全局样式文件
+import '@/styles/index.less'
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
